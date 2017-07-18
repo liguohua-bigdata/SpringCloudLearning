4a99cfa18683a2def3a91902272d7b56baf8a67a
liguohua <liguohua@cloud-star.com.cn>
2017-07-18 15:04:35 +0800

    init spring cloud learning

---
+++ microservice-simple-provider-user/src/main/java/com/cloudstar/controller/UserController.java
@@ -0,0 +1,21 @@
+package com.cloudstar.controller;
+
+import com.cloudstar.entity.User;
+import com.cloudstar.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+public class UserController {
+    @Autowired
+    private UserRepository userRepository;
+
+    @GetMapping("/{id}")
+    public User findById(@PathVariable Long id) {
+        User findOne = this.userRepository.findOne(id);
+        return findOne;
+    }
+}
